1ace7e1bdd032964e18ebc4192efd5934e644437
stan <stan@stanbox.space>
2025-08-31 21:37:49 +0300

    m1l1 first

--- m1l1-first/src/main/kotlin/Main.kt
+++ m1l1-first/src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 package com.example
 
 fun main() {
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        println("i = $i")
-    }
+    println("Hello World")
 }
